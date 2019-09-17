527c7145ce0ac23b9ad1bb525ab403ad08e9fb43
tomoko <t0m0k05238188+mac@gmail.com>
2019-09-17 23:02:47 +0900

    fix

--- docs/sw.js
+++ docs/sw.js
@@ -1,30 +1,24 @@
-// // Cache name
-// const CACHE_NAME = 'pwa-sample-caches-v1';
-// // Cache targets
-// const urlsToCache = [
-//   './vuguapp',
-//   './vuguapp/index.html',
-//   './vuguapp/main.wasm',
-//   './vuguapp/wasm_exec.js',
-//   './vuguapp/image/doc192.png',
-// ];
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = ['.'];
 
-// self.addEventListener('install', (event) => {
-//   event.waitUntil(
-//     caches
-//       .open(CACHE_NAME)
-//       .then((cache) => {
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-// self.addEventListener('fetch', (event) => {
-//   event.respondWith(
-//     caches
-//       .match(event.request)
-//       .then((response) => {
-//         return response ? response : fetch(event.request);
-//       })
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
